592f4c4f4bf1d4d52fe526b8bd747dad07447344
Azib Norazman <azib.naufal@gmail.com>
2025-03-31 09:18:41 +0100

    three layer

--- stella/optimise.py
+++ stella/optimise.py
@@ -6,11 +6,12 @@ def create_model_with_params(cnn_instance, params):
 
     
     """Create model with specified parameters"""
-    filter1 = 16
-    filter2 = 64
+    filter1 = 64
+    filter2 = 128
     filter3 = 256
     kernel_size1 = 7
-    kernel_size2 = 3
+    kernel_size2 = 7
+    kernel_size3 = 7
     dilation1 = 1
     dilation2 = 2
     dilation3 = 4
@@ -19,7 +20,7 @@ def create_model_with_params(cnn_instance, params):
         tf.keras.layers.Conv1D(
             filters=filter1,
             kernel_size=kernel_size1,
-            #dilation_rate=dilation1,
+            dilation_rate=dilation1,
             activation="relu",
             padding="same",
             input_shape=(cnn_instance.cadences, 1),
@@ -30,7 +31,7 @@ def create_model_with_params(cnn_instance, params):
         tf.keras.layers.Conv1D(
             filters=filter2,
             kernel_size=kernel_size2,
-            #dilation_rate=dilation2,
+            dilation_rate=dilation2,
             activation="relu",
             padding="same",
             kernel_regularizer=tf.keras.regularizers.l2(params['l2_lambda'])
@@ -38,23 +39,22 @@ def create_model_with_params(cnn_instance, params):
         tf.keras.layers.MaxPooling1D(pool_size=2),
         tf.keras.layers.Dropout(params['dropout']),
 
-
-        # tf.keras.layers.Conv1D(
-        #     filters=filter3,
-        #     kernel_size=kernel_size,
-        #     dilation_rate=dilation3,
-        #     activation="relu",
-        #     padding="same",
-        #     kernel_regularizer=tf.keras.regularizers.l2(params['l2_lambda'])
-        # ),
-        # tf.keras.layers.MaxPooling1D(pool_size=2),
-        # tf.keras.layers.Dropout(params['dropout']),
-
-        # tf.keras.layers.GlobalAveragePooling1D(),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(32, activation="relu", 
-                            kernel_regularizer=tf.keras.regularizers.l2(params['l2_lambda'])),
+        tf.keras.layers.Conv1D(
+            filters=filter3,
+            kernel_size=kernel_size3,
+            dilation_rate=dilation3,
+            activation="relu",
+            padding="same",
+            kernel_regularizer=tf.keras.regularizers.l2(params['l2_lambda'])
+        ),
+        tf.keras.layers.MaxPooling1D(pool_size=2),
         tf.keras.layers.Dropout(params['dropout']),
+
+        tf.keras.layers.GlobalAveragePooling1D(),
+        # tf.keras.layers.Flatten(),
+        # tf.keras.layers.Dense(32, activation="relu", 
+        #                     kernel_regularizer=tf.keras.regularizers.l2(params['l2_lambda'])),
+        # tf.keras.layers.Dropout(params['dropout']),
         tf.keras.layers.Dense(1, activation="sigmoid"),
     ])
 
